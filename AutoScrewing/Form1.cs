67f76316c700b387c0ed2a2acdd5eda16a28a2bd
admindpe-dharma <admindpe@dharmapacific.com>
2025-05-25 13:15:14 +0700

    add read

--- AutoScrewing/Form1.cs
+++ AutoScrewing/Form1.cs
@@ -17,26 +17,44 @@ namespace AutoScrewing
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Task.Run(async () => {
+                while (true) {
+                    try
+                    {
+                        using (_client)
+                        {
+                            await _client2.ConnectAsync(IPAddress.Parse(baseAddress), port);
+                            byte[] rd = new byte[2048];
+                            await _client2.GetStream().ReadAsync(rd, 0, rd.Length);
+                            string text = Encoding.ASCII.GetString(rd);
+                            await listBox1.InvokeAsync(() =>
+                            {
+                                listBox1.Items.Add(text);
+                            });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine(ex.Message);
+                    }
+                }
+            });
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
             using (_client)
             {
-                await _client.ConnectAsync(IPAddress.Parse(baseAddress),port);
+                await _client.ConnectAsync(IPAddress.Parse(baseAddress), port);
                 byte[] cmd = Encoding.ASCII.GetBytes(cmdText.Text);
-                using (var stream =  _client.GetStream())
+                await _client.GetStream().WriteAsync(cmd, 0, cmd.Length);
+                byte[] rd = new byte[2048];
+                await _client.GetStream().ReadAsync(rd, 0, rd.Length);
+                string text = Encoding.ASCII.GetString(rd);
+                await listBox1.InvokeAsync(() =>
                 {
-                    await stream.WriteAsync(cmd, 0, cmd.Length);
-                    byte[] rd = new byte[2048];
-                    await stream.ReadExactlyAsync(rd, 0, rd.Length);
-                    string text = Encoding.ASCII.GetString(rd);
-                    await listBox1.InvokeAsync(() =>
-                    {
-                        listBox1.Items.Add(text);
-                    });
-                }
+                    listBox1.Items.Add(text);
+                });
             }
         }
     }
