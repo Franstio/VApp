d7a1e5f9fb29b59478c725f5305a580a51470b67
admindpe-dharma <admindpe@dharmapacific.com>
2025-05-25 12:17:45 +0700

    init

---
+++ AutoScrewing/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AutoScrewing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmdText = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // cmdText
+            // 
+            cmdText.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            cmdText.Location = new Point(12, 88);
+            cmdText.Name = "cmdText";
+            cmdText.Size = new Size(443, 23);
+            cmdText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(461, 88);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Execute";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 117);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(583, 319);
+            listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(cmdText);
+            Name = "Form1";
+            Text = "Test Run";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox cmdText;
+        private Button button1;
+        private ListBox listBox1;
+    }
+}
---
+++ AutoScrewing/Form1.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AutoScrewing
+{
+    public partial class Form1 : Form
+    {
+        TcpClient _client = new TcpClient();
+        string baseAddress = "192.168.0.7";
+        int port = 8501;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (_client)
+            {
+                await _client.ConnectAsync(IPAddress.Parse(baseAddress),port);
+                byte[] cmd = Encoding.UTF8.GetBytes(cmdText.Text);
+                using (var stream =  _client.GetStream())
+                {
+                    await stream.WriteAsync(cmd, 0, cmd.Length);
+                    byte[] rd = new byte[2048];
+                    await stream.ReadExactlyAsync(rd, 0, rd.Length);
+                    string text = Encoding.UTF8.GetString(rd);
+                    await listBox1.InvokeAsync(() =>
+                    {
+                        listBox1.Items.Add(text);
+                    });
+                }
+            }
+        }
+    }
+}
