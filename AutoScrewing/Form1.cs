b0f26d0edc606e3f98b127daf7b3088f4e76d180
admindpe-dharma <admindpe@dharmapacific.com>
2025-05-27 15:41:39 +0700

    Ok

--- AutoScrewing/Form1.cs
+++ AutoScrewing/Form1.cs
@@ -59,7 +59,7 @@ namespace AutoScrewing
         {
             button1.Enabled = false;
             cts.Cancel();
-            barrier.SignalAndWait();
+            barrier.SignalAndWait(1000);
             DateTime dt = DateTime.Now;
             int checksum = dt.Year + dt.Month + dt.Day + dt.Hour + dt.Month + dt.Second;
             string command = $"{{{cmdText.Text},{dt.Year},{dt.Month},{dt.Date},{dt.Hour},{dt.Minute},{dt.Second}.{checksum},{checksum + 5438},1,1,}}\n\r";
