472d60c15673d252fe2821b421fe0d7095f5a5b6
admindpe-dharma <admindpe@dharmapacific.com>
2025-07-24 10:09:51 +0700

    OK

--- AutoScrewing/DashboardForm.cs
+++ AutoScrewing/DashboardForm.cs
@@ -199,15 +199,17 @@ namespace AutoScrewing
                     //    await logRepository.RecordLog(log);
                     //    continue;
                     //}
-
-                    bool check = FinalQueue.TryDequeue(out OngoingItemModel? item);
-                    if (!check || item is null) continue;
+                    if (FinalQueue.Count < 1)
+                        continue;
+                    var item = FinalQueue.Peek();
+                    if (item is null) continue;
                     item.FinalResult = item.ScrewingResult && item.LaserResult && item.CameraResult ? "OK" : "NG";
                     item.CurrentStatus = "Completed";
                     await meshController.Tracking(item.OperationUserSN,workNumberScanBox.Text, item.Scan_ID, item.Scan_ID2, item.FinalResult,item); ;
                     //                    var payload = new { serialnumber = item.Scan_ID, status = item.FinalResult, data = (TransactionModel)item };
                     //                    await File.WriteAllTextAsync(Path.Combine(path, "OUTPUT.txt"), JsonSerializer.Serialize(payload));
                     await TransactionRepository.CreateTransaction(item);
+                    FinalQueue.Dequeue();
                 }
                 catch (Exception ex)
                 {
