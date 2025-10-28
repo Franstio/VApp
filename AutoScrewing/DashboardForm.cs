a591c24822f89ddb96891873de5f758750a2e685
Franstio <franstiodiego01@gmail.com>
2025-10-28 16:52:07 +0700

    OK

--- AutoScrewing/DashboardForm.cs
+++ AutoScrewing/DashboardForm.cs
@@ -288,6 +288,7 @@ namespace AutoScrewing
                             {
                                 listView1.Items.Add($"{DateTime.Now.ToString("HH:mm:ss")}\t{res.message}");
                                 listView1.Items[listView1.Items.Count - 1].ForeColor = res.code == -1 ? ColorTranslator.FromHtml("#EF4444") : Color.White;
+                                listView1.Items.Add("");
                             });
                         }
                     }
