ad2b6cfdb4dd64e233de2c0b1a5dd10f9ef4b51e
Franstio <franstiodiego01@gmail.com>
2025-12-08 15:06:51 +0700

    OK

--- AutoScrewing/Dialogue/TestManualForm.cs
+++ AutoScrewing/Dialogue/TestManualForm.cs
@@ -22,25 +22,43 @@ namespace AutoScrewing.Dialogue
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            await InvokeAsync(()=>button1.Enabled=false);
             await PLCController.Send(new PLCController.PLCItem("WR", "MR2001", 1, ""));
+            await Task.Delay(100);
+            await PLCController.Send(new PLCController.PLCItem("WR", "MR2001", 0, ""));
+            await InvokeAsync(() => button1.Enabled = true);
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
 
+            await InvokeAsync(() => button2.Enabled = false);
             await PLCController.Send(new PLCController.PLCItem("WR", "MR2002", 1, ""));
+            await Task.Delay(100);
+            await PLCController.Send(new PLCController.PLCItem("WR", "MR2002", 0, ""));
+
+            await InvokeAsync(() => button2.Enabled = true);
         }
 
         private async void button3_Click(object sender, EventArgs e)
         {
 
+            await InvokeAsync(() => button3.Enabled = false);
             await PLCController.Send(new PLCController.PLCItem("WR", "MR2003", 1, ""));
+            await Task.Delay(100);
+            await PLCController.Send(new PLCController.PLCItem("WR", "MR2003", 0, ""));
+            await InvokeAsync(() => button3.Enabled = false);
         }
 
         private async void button4_Click(object sender, EventArgs e)
         {
 
+            await InvokeAsync(() => button4.Enabled = false);
             await PLCController.Send(new PLCController.PLCItem("WR", "MR2004", 1, ""));
+            await Task.Delay(100);
+            await PLCController.Send(new PLCController.PLCItem("WR", "MR2004", 0, ""));
+
+            await InvokeAsync(() => button4.Enabled = false);
         }
     }
 }
