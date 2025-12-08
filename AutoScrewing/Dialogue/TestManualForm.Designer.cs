bc1ee85552cbc3c241b0e15c4d2b5b99baa1acba
Franstio <franstiodiego01@gmail.com>
2025-12-08 15:53:19 +0700

    OK

--- AutoScrewing/Dialogue/TestManualForm.Designer.cs
+++ AutoScrewing/Dialogue/TestManualForm.Designer.cs
@@ -186,7 +186,7 @@
             button2.Name = "button2";
             button2.Size = new Size(391, 78);
             button2.TabIndex = 6;
-            button2.Text = "Jig Front";
+            button2.Text = "Jig Centering Front";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click_1;
             // 
